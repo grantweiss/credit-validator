6bee32c0cd0bc756121d59ca748236a3152cd424
Grant Weiss <grant.sweiss@gmail.com>
2024-11-27 12:17:05 -0600

    add test fetch route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [data, setData] = useState(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`${import.meta.env.VITE_API}/some-data`);
+      const data = await response.json();
+      setData(data);
+    };
+    fetchData();
+  }, []);
+
+  console.log(data);
   return (
     <>
       <div>
@@ -29,7 +40,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
-const app = express();
+const cors = require('cors')
+const bodyParser = require('body-parser');
+
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/', (req, res) => {
-  res.send('Hello World');
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+
+app.use('/some-data', (req, res) => {
+  res.json({ message: "Hello World" });
 });
 
 app.listen(3000, () => {
