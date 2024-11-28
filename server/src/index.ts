44c6d8f65f2e1f2e9a591c8f66b1a41a6e1276c4
Grant Weiss <grant.sweiss@gmail.com>
2024-11-28 08:31:13 -0600

    update api to ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,17 @@
-const express = require('express');
-const cors = require('cors')
-const bodyParser = require('body-parser');
-
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors())
+app.use(cors());
 
-app.use('/some-data', (req, res) => {
+app.use("/some-data", (req, res) => {
   res.json({ message: "Hello World" });
+  console.log("test");
 });
 
 app.listen(3000, () => {
